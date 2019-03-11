ad8176cfe9756c07fa78c94c3979c454d9ea019d
katherine <kretamales280@gmail.com>
2019-03-11 00:31:59 -0300

    Se agrego la configuracion de firebase de la wep app en el archivo environment.ts

--- BurgerQueen/src/environments/environment.ts
+++ BurgerQueen/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDvqTtA6pCDM-pzfmzGUtPlgNi7_4MtiSI",
+    authDomain: "burgerqueen-2b299.firebaseapp.com",
+    databaseURL: "https://burgerqueen-2b299.firebaseio.com",
+    projectId: "burgerqueen-2b299",
+    storageBucket: "burgerqueen-2b299.appspot.com",
+    messagingSenderId: "202918577928"
+  }
 };
 
 /*
