247836c3d83cb7836b54fefc63086ce8c5f93d4e
katherine <kretamales280@gmail.com>
2019-03-11 00:47:12 -0300

    Se cambiaron las comillas  por ''

--- BurgerQueen/src/environments/environment.ts
+++ BurgerQueen/src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDvqTtA6pCDM-pzfmzGUtPlgNi7_4MtiSI",
-    authDomain: "burgerqueen-2b299.firebaseapp.com",
-    databaseURL: "https://burgerqueen-2b299.firebaseio.com",
-    projectId: "burgerqueen-2b299",
-    storageBucket: "burgerqueen-2b299.appspot.com",
-    messagingSenderId: "202918577928"
+    apiKey: 'AIzaSyDvqTtA6pCDM-pzfmzGUtPlgNi7_4MtiSI',
+    authDomain: 'burgerqueen-2b299.firebaseapp.com',
+    databaseURL: 'https://burgerqueen-2b299.firebaseio.com',
+    projectId: 'burgerqueen-2b299',
+    storageBucket: 'burgerqueen-2b299.appspot.com',
+    messagingSenderId: '202918577928'
   }
 };
 
